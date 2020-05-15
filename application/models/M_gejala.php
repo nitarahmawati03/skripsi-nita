3740778bd7ff883c0643c8ad365ad39f20906e05
Nita Rahmawati Komalasari <44804999+nitarahmawati03@users.noreply.github.com>
2020-05-15 22:59:10 +0700

    Create M_gejala.php

---
+++ application/models/M_gejala.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_gejala extends CI_Model {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function readGejala(){
+		$query=$this->db->get('tb_gejala');
+		return $query->result();
+	}
+
+	public function insertGejala(){
+		$nextId = '';
+		$query = $this->db->select('id_gejala')
+                      ->from('tb_gejala')
+                      ->get();
+		$row = $query->last_row();
+		if($row){
+			$idPostfix = (int)substr($row->id_gejala,1)+1;
+			$nextId = 'P'.STR_PAD((string)$idPostfix,2,"0",STR_PAD_LEFT);
+		}
+		else{
+			$nextId = 'G01';
+		} // For the first time
+			$object=array
+			(
+				'id_gejala' => $nextId,
+				'gejala'=>$this->input->post('gejala'),
+				'id_bobot'=>$this->input->post('id_bobot'),
+			);
+		$this->db->insert('tb_gejala',$object);
+	}
+
+}
