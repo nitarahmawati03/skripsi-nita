7e0eedae5448cdc6aa90679bf15855b34d8abc10
Nita Rahmawati Komalasari <44804999+nitarahmawati03@users.noreply.github.com>
2020-05-23 14:23:14 +0700

    folder datatable

--- application/views/tabelgejala.php
+++ application/views/tabelgejala.php
@@ -46,3 +46,29 @@
         </div>
     </div>
 </div>
+
+ <!--   Core JS Files   -->
+    <script src="<?php echo base_url()?>assets/js/3.2.1/jquery.js" type="text/javascript"></script>
+    <script src="<?php echo base_url()?>assets/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="<?=base_url()?>DataTables/datatables.min.js" type="text/javascript"></script>
+    <!--  Charts Plugin -->
+    <!-- <script src="<?php echo base_url()?>assets/js/chartist.min.js"></script>
+
+     Notifications Plugin   
+    <script src="<?php echo base_url()?>assets/js/bootstrap-notify.js"></script>
+ -->
+    <!--  Google Maps Plugin    -->
+    <script type="<?php echo base_url()?>text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+    <!-- <script src="<?php echo base_url()?>assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script> -->
+
+    <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+    <!-- <script src="<?php echo base_url()?>assets/js/demo.js"></script> -->
+  
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('#contoh').DataTable();
+    });
+  </script>
+ 
