6772a5923586ab81cacc98744ae53aaceb8ee121
Nita Rahmawati Komalasari <44804999+nitarahmawati03@users.noreply.github.com>
2020-06-15 10:43:44 +0700

    coba

--- application/controllers/Pakar.php
+++ application/controllers/Pakar.php
@@ -4,11 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Pakar extends CI_Controller {
 
 
-	function __construct(){
+	public function __construct(){
 		parent::__construct();
 		$this->load->library(array('form_validation','session'));
 
-		$this->load->model('M_Pakar');
+		$this->load->model('M_pakar');
 		if(!$this->session->userdata('level'))
 		{
 			redirect('Pengelola/menulogin');
