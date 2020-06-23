d160ab41105f63b4d27fb10201b33b6f74db2d3d
Nita Rahmawati Komalasari <44804999+nitarahmawati03@users.noreply.github.com>
2020-06-23 22:00:43 +0700

    hitung

--- application/controllers/Perhitungan.php
+++ application/controllers/Perhitungan.php
@@ -3,6 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Perhitungan extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url','form');
+		$this->load->library('form_validation');
+	}
 
 	public function index()
 	{
